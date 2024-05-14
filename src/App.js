704231649b49b88b528fe44b1ffdcbafdebd6cb4
Ted@Crestwood <ted@crestwood.co.ke>
2024-05-14 16:32:58 +0300

    starter commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex bg-red-500">
+      <div>Start</div>
+      <div>Second</div>
+    </div>
+  );
+}
+
+export default App;
